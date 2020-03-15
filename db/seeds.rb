f2e6fe93e9c2ed7c950c270f9bc5a187a9b91774
samidahlawi <o.t5@hotmail.com>
2020-03-15 21:34:25 +0300

    Add user devise and group model with relation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Group.destroy_all
+
+Group.create({name:"Saudi Arabia"})
+Group.create({name:"US"})
+Group.create({name:"General"})
