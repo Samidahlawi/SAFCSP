04b8d51f58b306b85eeb04eab68a11c6e16d7206
samidahlawi <o.t5@hotmail.com>
2020-03-17 23:16:54 +0300

    add the checkbox 0f groups

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,18 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 
+$(function(){
+    var requiredCheckboxes = $('.options :checkbox[required]');
+    requiredCheckboxes.change(function(){
+        if(requiredCheckboxes.is(':checked')) {
+            requiredCheckboxes.removeAttr('required');
+        } else {
+            requiredCheckboxes.attr('required', 'required');
+        }
+    });
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
